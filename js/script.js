c679a8d6b0d71f788d071ee2eb65f4a8dd8ecc90
ArtiomB5 <74674499+ArtiomB5@users.noreply.github.com>
2021-04-14 20:33:54 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let logoLink = document.getElementById("logo_link");
+logoLink.onclick = function() {
+    window.location.href='https://www.google.com';
+};
+
+let cupsLink = document.getElementById("cups_link");
+cupsLink.onclick = function() {
+    window.location.href='https://www.google.com';
+};
+
+let womanLink = document.getElementById("woman_link");
+womanLink.onclick = function() {
+    window.location.href='https://www.google.com';
+};
+
+let tshirtsLink = document.getElementById("tshirts_link");
+tshirtsLink.onclick = function() {
+    window.location.href='https://www.google.com';
+};
